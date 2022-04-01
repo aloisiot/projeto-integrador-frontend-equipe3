2a377f5416438fc38c9c14547a18fe22a5a661b0
Arthur <arthurlacerda206@gmail.com>
2022-04-01 11:30:28 -0300

    integracoes na pagina de reserva

--- src/app/auth/AuthContext.jsx
+++ src/app/auth/AuthContext.jsx
@@ -5,9 +5,8 @@ import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const AppContext = createContext();
-
 export function AuthProvider(props) {
-    const cookieName = "c7d5e61a-e190-43b0-b422-b56571c0005d"
+    const userCookieName = "c7d5e61a-e190-43b0-b422-b56571c0005d"
     const navigate = useNavigate()
     const [ authenticated, setAuthenticated ] = useState()
 
@@ -20,7 +19,7 @@ export function AuthProvider(props) {
     }, [authenticated, checkIsAuthenticated])
 
     async function signIn(email, password, keepConnected, onRejected) {
-        jsCookie.remove(cookieName)
+        jsCookie.remove(userCookieName)
         const resp = await axios.post(
             `${process.env.REACT_APP_LINK_API}/auth/sign-in`,
             {email, password}
@@ -28,7 +27,7 @@ export function AuthProvider(props) {
         
         if(resp.status === 200) {
             const options = keepConnected ? { expires: 30 } : undefined
-            jsCookie.set(cookieName, JSON.stringify(resp.data), options)
+            jsCookie.set(userCookieName, JSON.stringify(resp.data), options)
             setAuthenticated(true)
         } else {
             onRejected()
@@ -50,12 +49,12 @@ export function AuthProvider(props) {
     } 
 
     function signOut() {
-        jsCookie.remove(cookieName)
+        jsCookie.remove(userCookieName)
         setAuthenticated(false)
     }
 
     function getUserDetails() {
-        const cookie = jsCookie.get(cookieName)
+        const cookie = jsCookie.get(userCookieName)
         if(cookie) {
             const cookieContent = JSON.parse(cookie)
             return cookieContent.userDetails
@@ -63,7 +62,7 @@ export function AuthProvider(props) {
     }
 
     function getTocken() {
-        const cookie = JSON.parse(jsCookie.get(cookieName))
+        const cookie = JSON.parse(jsCookie.get(userCookieName))
         const tocken =  `${cookie.type} ${cookie.token}`
         return tocken
     }
--- src/utilitarios/formatarData.js
+++ src/utilitarios/formatarData.js
@@ -1,6 +1,7 @@
 export default function formatarData(data) {
-    let result = ""
-    const mes = result.concat(String(data.getMonth()).length > 1 ? data.getMonth() : "0" + data.getMonth())
-    const dia = result.concat(String(data.getDate()).length > 1 ? data.getDate() : "0" + data.getDate())
-    return `${dia}/${mes}/${data.getFullYear()}`
+    return data.toLocaleDateString('pt-BR')
+}
+
+export function formatDateForTransfer(data) {
+    return formatarData(data).split("/").reverse().join("-")
 }
