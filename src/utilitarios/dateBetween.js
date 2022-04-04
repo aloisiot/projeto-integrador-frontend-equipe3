b375cc779f308010a5e0fe61f85439a1279fd80f
Arthur <arthurlacerda206@gmail.com>
2022-04-04 20:22:00 -0300

    comentario para revisao postuma

--- src/utilitarios/dateBetween.js
+++ src/utilitarios/dateBetween.js
@@ -1,3 +1,9 @@
+// comentário para uma futura revisão:
+// disableDate aqui tem uma função que retorna uma outra função que recebe os parametros de cada data no calendario. O props disableDay do DateRangePicker
+// passa cada data para essa função retornada, ao mesmo tempo que ela tem acesso ao dateRangeList, que são os intervalos de data reservados
+// (do dia 6 ao 10 por exemplo). Se a data no calendario tiver os milisegundos iguais ou maiores aos milisegundos totais da data de inicio do intervalo e
+// tambem tiver os milisegundos iguais ou menores a data de fim significará que ela está dentro do intervalo reservado e assim a função retornara true para
+// desativar a data.
 
 
  const  disableDate = (dateRangeList) => (date) => {
